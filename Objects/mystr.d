4536e53ebacd59ee35383681a6c11055d0c86b76
idrnyu <834999326@qq.com>
2019-10-27 14:41:47 +0800

    d

--- Objects/mystr.d
+++
@@ -1,4 +0,0 @@
-.\objects\mystr.o: Drives\src\mystr.c
-.\objects\mystr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\mystr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\mystr.o: .\Drives\inc\mystr.h
--- Objects/startup_stm32f10x_md_vl.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f10x_md_vl.o: RTE\Device\STM32F100C8\startup_stm32f10x_md_vl.s
