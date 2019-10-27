3214c305533225e4ba84d681cd06d52d89cbd469
idrnyu <834999326@qq.com>
2019-10-27 10:25:17 +0800

    test

---
+++ Objects/mystr.d
@@ -0,0 +1,4 @@
+.\objects\mystr.o: Drives\src\mystr.c
+.\objects\mystr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\mystr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\mystr.o: .\Drives\inc\mystr.h
